457f46db72cd14e7b061ee142f05e89c781d0db1
Marcus Tutorials <marcusharting@web.de>
2022-02-25 19:31:01 +0100

    Impressumsfix

--- src/views/Impressum.vue
+++ src/views/Impressum.vue
@@ -1,6 +1,22 @@
 <template>
 <div class="Impressum">
 
+    <teleport to="head">
+        <title>Impressum</title>
+
+        <meta
+            property="og:title"
+            content="Marcus Universe Impressum">
+
+        <meta
+            property="og:description"
+            content="Marcus Harting (alias Marcus Universe) Portfolio. Im an artist, musician and coder">
+
+        <meta
+            property="og:image"
+            content="./assets/img/me_profile.png">
+    </teleport>
+
     <router-link to="/">
         <Logo class="LogoImpressum" />
     </router-link>
@@ -10,23 +26,22 @@
     <p>Marcus Universe<br>Kommunikationsdesigner (Freiberuflich)<br>Marcus Harting</p>
 
     <h3>Kontakt:</h3>
-    <p>E-Mail: <a href="mailto:contact@marcus-universe.de">contact@marcus-universe.de</a>  (bevorzugt)</p>
+    <p>E-Mail: <a href="mailto:contact@marcus-universe.de">contact@marcus-universe.de</a> (bevorzugt)</p>
 
     <h3>Postanschrift:</h3>
     <p><b>Kunstschule Wandsbek (Antreffbar)</b></p>
     <p>Neumann-Reichardt-Str. 27–33, Haus 19<br>22041 Hamburg<br></p>
-    
-   
+
     <p><br></p>
     <p></p>
     <h2>Datenschutz / Hinweise zur Webseite</h2>
 
-<h3>Hoster</h3>
-<p>Diese Webseite läuft auf <a href="https://www.strato.de/" target="_blank" rel="noopener noreferrer">Strato</a>  Servern </p>
+    <h3>Hoster</h3>
+    <p>Diese Webseite läuft auf <a href="https://www.strato.de/" target="_blank" rel="noopener noreferrer">Strato</a> Servern </p>
 
-     <h3>Google Fonts</h3>
+    <h3>Google Fonts</h3>
     <p>Alle Fonst/Schriftarten sind lokal auf dem Server gespeichert und werden nicht von Google Servern runtergeladen.</p>
-<br>
+    <br>
     <p><strong>Haftungsausschluss: </strong><br><br><strong>Haftung für Inhalte</strong><br><br>
         Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.<br><br><strong>Haftung für Links</strong><br><br>
         Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.<br><br><strong>Urheberrecht</strong><br><br>
