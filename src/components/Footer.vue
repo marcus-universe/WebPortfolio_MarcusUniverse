48e5dc051bb8262f9b8878666857ab45eafcdb42
Marcus Tutorials <marcusharting@web.de>
2022-11-22 16:06:31 +0100

    Added Share Button and use Hash Routes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,13 @@
 <template>
 <footer>
 
-    <router-link
-        to="/impressum"
-        class="flex_c_h impressumButton">
-        Impressum
-
-    </router-link>
+    <div class="flex_c_h">
+        <router-link to="/impressum" class="flex_c_h impressumButton">
+            Impressum / Datapolicy
+        
+        </router-link>
+    </div>
+    
 
     <div class="social_media gap2">
         <a class="flex_c" target="_blank" href="https://www.artstation.com/marcusharting"><img src="../assets/artstation.svg" alt="Artstation">
@@ -20,12 +21,17 @@
         </a>
 
     </div>
+
+    <div class="flex_c_h" >
+        <ShareButton />
+    </div>
+
     <div class="flex_c_h">
         <div class="CreditSection GithubRepo">
             <a class="flex_c" target="_blank" href="https://github.com/marcus-universe/WebPortfolio_MarcusUniverse">
-                Github Repo 
+                Github Repo
             </a>
-            </div>
+        </div>
     </div>
 
     <div class="CreditSection">©2022 coded by Marcus Harting </div>
@@ -34,6 +40,14 @@
 </template>
 
 <script>
+import ShareButton from '@/components/Ui/ShareButton.vue'
+export default {
+    name: 'Footer',
+    components: {
+        ShareButton
+    },
+}
+
 </script>
 
 <style lang="scss">
