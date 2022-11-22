4eea39fece9330b47efe765bd97302791767dc24
Marcus Tutorials <marcusharting@web.de>
2022-11-22 12:33:09 +0100

    Footer Github Repo + fixed Icons

--- src/components/ErrorPage.vue
+++ src/components/ErrorPage.vue
@@ -11,7 +11,6 @@
             <a href="https://www.artstation.com/marcusharting"><img src="../assets/artstation.svg" alt="Artstation"></a>
             <a href="https://www.instagram.com/marcustutorials/"><img src="../assets/insta.svg" alt="Instagram"></a>
             <a href="https://www.youtube.com/user/MarcusTutorials1"><img src="../assets/yt.svg" alt="YouTube"></a>
-
         </div>
 
 
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,25 +1,41 @@
 <template>
 <footer>
 
-<router-link to="/impressum" class="flex_c_h impressumButton">
-    Impressum
+    <router-link
+        to="/impressum"
+        class="flex_c_h impressumButton">
+        Impressum
 
-</router-link>
+    </router-link>
 
-    <div class="social_media">
-            <a target="_blank" href="https://www.artstation.com/marcusharting"><img src="../assets/artstation.svg" alt="Artstation"></a>
-            <a target="_blank" href="https://www.instagram.com/marcusunivers/"><img src="../assets/insta.svg" alt="Instagram"></a>
-            <a target="_blank" href="https://www.youtube.com/c/MarcusUniverse-Official"><img src="../assets/yt.svg" alt="YouTube"></a>
+    <div class="social_media gap2">
+        <a class="flex_c" target="_blank" href="https://www.artstation.com/marcusharting"><img src="../assets/artstation.svg" alt="Artstation">
+            <p>Artstation</p>
+        </a>
+        <a class="flex_c" target="_blank" href="https://www.instagram.com/marcusunivers/"><img src="../assets/insta.svg" alt="Instagram">
+            <p>Instagram</p>
+        </a>
+        <a class="flex_c" target="_blank" href="https://youtube.com/@marcusunivers"><img src="../assets/yt.svg" alt="YouTube">
+            <p>YouTube</p>
+        </a>
 
+    </div>
+    <div class="flex_c_h">
+        <div class="CreditSection GithubRepo">
+            <a class="flex_c" target="_blank" href="https://github.com/marcus-universe/WebPortfolio_MarcusUniverse">
+                Github Repo 
+            </a>
+            </div>
     </div>
 
     <div class="CreditSection">©2022 coded by Marcus Harting </div>
 
-    
 </footer>
 </template>
 
-<style lang="scss">
+<script>
+</script>
 
+<style lang="scss">
 
 </style>
